f77df424c4c4de3f24dce1fa5df1cfeddaa6dda8
nabi-design <aadedoyinsola6@gmail.com>
2024-09-15 22:29:37 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+ // JavaScript Smooth Scroll Function
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
